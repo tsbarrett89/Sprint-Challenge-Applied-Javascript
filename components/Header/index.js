bec287ef310c2b7214e296205f1577e27bc6dab9
Thomas Barrett <tsbarrett89@gmail.com>
2019-10-04 09:51:31 -0600

    finsihed header assignment

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,26 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+    const header = document.createElement('div');
+    const date = document.createElement('span');
+    const headerOne = document.createElement('h1');
+    const temp = document.createElement('span');
+
+    header.appendChild(date)
+    header.appendChild(headerOne)
+    header.appendChild(temp)
+
+    header.classList.add('class')
+    date.classList.add('date')
+    temp.classList.add('temp')
+
+    date.textContent = 'Smarch 28, 2019';
+    headerOne.textContent = 'Lambda Times';
+    temp.textContent = '98°';
+
+    return header;
+}
+
+const headerContainer = document.querySelector('.header-container')
+headerContainer.appendChild(Header())
